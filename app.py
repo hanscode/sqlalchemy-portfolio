ca384e54854db6084e5ec4a400ecac4e5bc6f66b
Hans Steffens <hans@steffens.co>
2025-05-06 17:49:11 -0500

    Layout structure and templates inheritance

--- app.py
+++ app.py
@@ -1,10 +1,27 @@
-from flask import Flask
+from flask import Flask, render_template, url_for, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hello from my portfolio web app!"
+    return render_template('index.html')
+
+
+@app.route('/new', methods=['GET', 'POST'])
+def add_project():
+    print(request.form)
+    return render_template('new.html')
+
+
+@app.route('/projects')
+def project():
+    return render_template('detail.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
 
 
 if __name__ == '__main__':
